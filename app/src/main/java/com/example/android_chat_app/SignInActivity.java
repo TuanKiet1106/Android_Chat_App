dccba5f68561eb151727d0c1129e3755b5819166
Kiet <20520906@gm.uit.edu.vn>
2023-04-11 17:26:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_chat_app/SignInActivity.java
@@ -0,0 +1,27 @@
+package com.example.android_chat_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.android_chat_app.databinding.ActivitySignInBinding;
+
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener( v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/example/android_chat_app/SignUpActivity.java
@@ -0,0 +1,25 @@
+package com.example.android_chat_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.android_chat_app.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
