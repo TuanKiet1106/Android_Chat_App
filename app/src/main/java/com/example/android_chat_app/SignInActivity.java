9455d1184feb2ba972942098af2bf2b818dbaefe
Kiet <20520906@gm.uit.edu.vn>
2023-04-12 12:37:53 +0700

    - Finish SignIn & SignUp Layout. - Finish Connecting to Firebase Database. - Finish Cloud Commuting. - Finish SignUp Functions.

--- app/src/main/java/com/example/android_chat_app/SignInActivity.java
+++ app/src/main/java/com/example/android_chat_app/SignInActivity.java
@@ -1,9 +1,9 @@
 package com.example.android_chat_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android_chat_app.databinding.ActivitySignInBinding;
 
